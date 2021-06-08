68f4088b5b6e59702307e62c860791902323d5b5
Jared Landers <patlymlatun@gmail.com>
2021-06-07 22:36:15 -0700

    First commit.

---
+++ target/debug/deps/acpi_rs-07b39e980be26269.d
@@ -0,0 +1,5 @@
+/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-07b39e980be26269.rmeta: src/lib.rs
+
+/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-07b39e980be26269.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/acpi_rs-40ceb2d2d184bf71.d
@@ -0,0 +1,5 @@
+/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-40ceb2d2d184bf71.rmeta: src/lib.rs
+
+/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-40ceb2d2d184bf71.d: src/lib.rs
+
+src/lib.rs:
