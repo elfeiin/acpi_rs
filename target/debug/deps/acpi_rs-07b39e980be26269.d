b363b2101c4af507938a4bd0a6f7c13225577c98
anyusernameworks <patlymlatun@gmail.com>
2021-06-07 22:38:45 -0700

    small refactor

--- target/debug/deps/acpi_rs-07b39e980be26269.d
+++
@@ -1,5 +0,0 @@
-/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-07b39e980be26269.rmeta: src/lib.rs
-
-/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-07b39e980be26269.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/acpi_rs-40ceb2d2d184bf71.d
+++
@@ -1,5 +0,0 @@
-/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-40ceb2d2d184bf71.rmeta: src/lib.rs
-
-/home/elfein/Projects/acpi_rs/target/debug/deps/acpi_rs-40ceb2d2d184bf71.d: src/lib.rs
-
-src/lib.rs:
